252b4303b37e7080c03936f47b7204ccdedbc9aa
xisTC <536440296@qq.com>
2025-11-01 15:07:21 +0800

    Initial commit

---
+++ ModBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Duckov.Modding;
+using UnityEngine;
+
+// 测试ModSetting的保存功能
+namespace ModSettingTest1 {
+    public class ModBehaviour : Duckov.Modding.ModBehaviour{
+        private void OnEnable() {
+            ModManager.OnModActivated += ModManager_OnModActivated;
+        }
+        private void OnDisable() {
+            ModManager.OnModActivated -= ModManager_OnModActivated;
+            Setting.Clear();
+        }
+        private void AddUI() {
+            ModSettingAPI.AddDropdownList("D1", "下拉列表1",
+                new List<string> { "选项1", "选项2", "选项3" }, 
+                Setting.Dropdown1, Setting.SetDropdown1);
+            ModSettingAPI.AddDropdownList("D2", "下拉列表2", 
+                new List<string> { "选项7", "选项8", "选项9" },
+                Setting.Dropdown2, Setting.SetDropdown2);
+            ModSettingAPI.AddToggle("T1", "按钮1", Setting.Toggle1, Setting.SetToggle1);
+            ModSettingAPI.AddToggle("T2", "按钮2", Setting.Toggle2, Setting.SetToggle2);
+            ModSettingAPI.AddSlider("S1", "滑块1", Setting.Slider1, new Vector2(0, 100), Setting.SetSlider1);
+            ModSettingAPI.AddSlider("S2", "滑块2", Setting.Slider2, new Vector2(0, 1000), Setting.SetSlider2,2);
+            ModSettingAPI.AddSlider("S3", "滑块3", 60, new Vector2(0, 1000), null,3,8);
+            ModSettingAPI.AddSlider("S4", "滑块4", 50,0,200,value=>{ Debug.Log("滑块4:"+value);});
+            ModSettingAPI.AddInput("I1", "输入框1", Setting.Input1, 40, Setting.SetInput1);
+            ModSettingAPI.AddInput("I2", "输入框2", Setting.Input2, 50, Setting.SetInput2);
+            ModSettingAPI.AddKeybinding("K1", "按键绑定1", Setting.Keybinding1, Setting.SetKeybinding1);
+            ModSettingAPI.AddKeybinding("K2", "按键绑定2", Setting.Keybinding2, Setting.SetKeybinding2);
+            
+            ModSettingAPI.AddToggle("T3", "点击移除S2", false, value => {
+                ModSettingAPI.RemoveUI("S2", result => { Debug.Log($"移除{(result?"成功":"失败")}");
+                });
+            });
+            
+        }
+        private void ModManager_OnModActivated(ModInfo arg1, Duckov.Modding.ModBehaviour arg2) {
+            if (arg1.name != ModSettingAPI.MOD_NAME || !ModSettingAPI.Init(info)) return;
+            //(触发时机:此mod在ModSetting之前启用)检查启用的mod是否是ModSetting,是进行初始化
+            //先从ModSetting中读取配置
+            Setting.Init();
+            AddUI();
+        }
+        protected override void OnAfterSetup() {
+            //(触发时机:ModSetting在此mod之前启用)此mod，Setup后,尝试进行初始化
+            if (!ModSettingAPI.Init(info)) return;
+            //先从ModSetting中读取配置
+            Setting.Init();
+            AddUI();
+        }
+    }
+}
---
+++ Setting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace ModSettingTest1 {
+    public static class Setting {
+        public static string Dropdown1 { get; private set; }
+        public static string Dropdown2 { get; private set; }
+        public static bool Toggle1 { get; private set; }
+        public static bool Toggle2 { get; private set; }
+        public static float Slider1 { get; private set; }
+        public static float Slider2 { get; private set; }
+        public static string Input1 { get; private set; }
+        public static string Input2 { get; private set; }
+        public static KeyCode Keybinding1 { get; private set; }
+        public static KeyCode Keybinding2 { get; private set; }
+        public static event Action<float> OnSlider1ValueChanged;
+        public static void SetDropdown1(string value) => Dropdown1 = value;
+        public static void SetDropdown2(string value) => Dropdown2 = value;
+        public static void SetToggle1(bool value) => Toggle1 = value;
+        public static void SetToggle2(bool value) => Toggle2 = value;
+        public static void SetSlider1(float value) {
+            Slider1 = value;
+            OnSlider1ValueChanged?.Invoke(value);
+        }
+
+        public static void SetSlider2(float value) => Slider2 = value;
+        public static void SetInput1(string value) => Input1 = value;
+        public static void SetInput2(string value) => Input2 = value;
+        public static void SetKeybinding1(KeyCode value) => Keybinding1 = value;
+        public static void SetKeybinding2(KeyCode value) => Keybinding2 = value;
+        
+        public static void Clear() {
+            OnSlider1ValueChanged = null;
+        }
+
+        public static void Init() {
+            if (ModSettingAPI.HasConfig()) {
+                Dropdown1 = ModSettingAPI.GetSavedValue("D1", out string d1) ? d1 : "选项2";
+                Dropdown2 = ModSettingAPI.GetSavedValue("D2", out string d2) ? d2 : "选项9";
+                Toggle1 = ModSettingAPI.GetSavedValue("T1", out bool t1) ? t1 : false;
+                Toggle2 = ModSettingAPI.GetSavedValue("T2", out bool t2) ? t2 : true;
+                Slider1 = ModSettingAPI.GetSavedValue("S1", out float s1) ? s1 : 0;
+                Slider2 = ModSettingAPI.GetSavedValue("S2", out float s2) ? s2 : 0;
+                Input1 = ModSettingAPI.GetSavedValue("I1", out string i1) ? i1 : "输入框1";
+                Input2 = ModSettingAPI.GetSavedValue("I2", out string i2) ? i2 : "输入框2";
+                Keybinding1 = ModSettingAPI.GetSavedValue("K1", out KeyCode k1) ? k1 : KeyCode.N;
+                Keybinding2 = ModSettingAPI.GetSavedValue("K2", out KeyCode k2) ? k2 : KeyCode.None;
+            } else {
+                //设置默认值
+                Dropdown1 = "选项2";
+                Dropdown2 = "选项9";
+                Toggle1 = false;
+                Toggle2 = true;
+                Keybinding1 = KeyCode.N;
+                Keybinding2 = KeyCode.None;
+            }
+        }
+    }
+}
