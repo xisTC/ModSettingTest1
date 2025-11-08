375ef7325ed63c74b07da0451ef9061b8f96c7e1
xisTC <536440296@qq.com>
2025-11-08 22:00:29 +0800

    更新API

--- Api/ModSettingAPI.cs
+++ Api/ModSettingAPI.cs
@@ -48,6 +48,11 @@ public static class ModSettingAPI {
         ADD_GROUP
     };
 
+    /// <summary>
+    /// 初始化API
+    /// </summary>
+    /// <param name="modInfo">mod信息</param>
+    /// <returns>是否成功初始化</returns>
     public static bool Init(ModInfo modInfo) {
         if (IsInit) return true;
         ModSettingAPI.modInfo = modInfo;
@@ -68,6 +73,15 @@ public static class ModSettingAPI {
         return true;
     }
 
+    /// <summary>
+    /// 添加一个下拉列表控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="options">选项列表</param>
+    /// <param name="defaultValue">当前值</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <returns></returns>
     public static bool AddDropdownList(string key, string description,
         List<string> options, string defaultValue, Action<string> onValueChange = null) {
         if (!Available(key)) return false;
@@ -78,6 +92,17 @@ public static class ModSettingAPI {
             delegateType);
     }
 
+    /// <summary>
+    /// 添加一个浮点数滑块控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="defaultValue">当前值</param>
+    /// <param name="sliderRange">滑块范围</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <param name="decimalPlaces">小数位数</param>
+    /// <param name="characterLimit">输入字符限制</param>
+    /// <returns></returns>
     public static bool AddSlider(string key, string description,
         float defaultValue, Vector2 sliderRange, Action<float> onValueChange = null, int decimalPlaces = 1,
         int characterLimit = 5) {
@@ -95,6 +120,17 @@ public static class ModSettingAPI {
             paramTypes);
     }
 
+    /// <summary>
+    /// 添加一个整数滑块控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="defaultValue">当前值</param>
+    /// <param name="minValue">最小值</param>
+    /// <param name="maxValue">最大值</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <param name="characterLimit">输入字符限制</param>
+    /// <returns></returns>
     public static bool AddSlider(string key, string description,
         int defaultValue, int minValue, int maxValue, Action<int> onValueChange = null, int characterLimit = 5) {
         if (!Available(key)) return false;
@@ -110,6 +146,14 @@ public static class ModSettingAPI {
             paramTypes);
     }
 
+    /// <summary>
+    /// 添加一个开关控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="enable">当前值</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <returns></returns>
     public static bool AddToggle(string key, string description,
         bool enable, Action<bool> onValueChange = null) {
         if (!Available(key)) return false;
@@ -120,6 +164,14 @@ public static class ModSettingAPI {
             delegateType);
     }
 
+    /// <summary>
+    /// 添加一个按键绑定控件，默认值None
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="keyCode">当前值</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <returns></returns>
     public static bool AddKeybinding(string key, string description,
         KeyCode keyCode, Action<KeyCode> onValueChange = null) {
         if (!Available(key)) return false;
@@ -129,7 +181,16 @@ public static class ModSettingAPI {
             typeof(Action<ModInfo, string, string, KeyCode, Action<KeyCode>>));
     }
 
-    public static bool AddKeybindingWithDefault(string key, string description,
+    /// <summary>
+    /// 添加一个带默认值的按键绑定控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="keyCode">当前值</param>
+    /// <param name="defaultKeyCode">默认值</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <returns></returns>
+    public static bool AddKeybinding(string key, string description,
         KeyCode keyCode, KeyCode defaultKeyCode, Action<KeyCode> onValueChange = null) {
         if (!Available(key)) return false;
         return InvokeMethod(ADD_KEYBINDING_WITH_DEFAULT,
@@ -138,6 +199,15 @@ public static class ModSettingAPI {
             typeof(Action<ModInfo, string, string, KeyCode, KeyCode, Action<KeyCode>>));
     }
 
+    /// <summary>
+    /// 添加一个输入框控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="defaultValue">当前值</param>
+    /// <param name="characterLimit">输入字符限制</param>
+    /// <param name="onValueChange">值改变时的回调函数</param>
+    /// <returns></returns>
     public static bool AddInput(string key, string description,
         string defaultValue, int characterLimit = 40, Action<string> onValueChange = null) {
         if (!Available(key)) return false;
@@ -147,6 +217,14 @@ public static class ModSettingAPI {
             typeof(Action<ModInfo, string, string, string, int, Action<string>>));
     }
 
+    /// <summary>
+    /// 添加一个按钮控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="buttonText">按钮文本</param>
+    /// <param name="onClickButton">点击时的回调函数</param>
+    /// <returns></returns>
     public static bool AddButton(string key, string description,
         string buttonText = "按钮", Action onClickButton = null) {
         if (!Available(key)) return false;
@@ -156,15 +234,32 @@ public static class ModSettingAPI {
             typeof(Action<ModInfo, string, string, string, Action>));
     }
 
+    /// <summary>
+    /// 添加一个分组控件，用于将多个控件组织在一起
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="description">描述文本</param>
+    /// <param name="keys">包含的控件键列表(暂不支持嵌套Group)</param>
+    /// <param name="scale">缩放比例(相对于mod标题)，最大0.9f</param>
+    /// <param name="topInsert">是否插入到顶部</param>
+    /// <param name="open">是否默认展开</param>
+    /// <returns></returns>
     public static bool AddGroup(string key, string description, List<string> keys,
-        float scale=0.7f, bool topInsert = false, bool open = false) {
+        float scale = 0.7f, bool topInsert = false, bool open = false) {
         if (!Available(key)) return false;
         return InvokeMethod(ADD_GROUP,
             ADD_GROUP,
-            new object[] { modInfo, key, description, keys, scale,topInsert,open},
-            typeof(Action<ModInfo, string, string, List<string>, float,bool,bool>));
+            new object[] { modInfo, key, description, keys, scale, topInsert, open },
+            typeof(Action<ModInfo, string, string, List<string>, float, bool, bool>));
     }
 
+    /// <summary>
+    /// 获取指定key的配置值
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="callback">回调函数返回结果</param>
+    /// <typeparam name="T">值类型</typeparam>
+    /// <returns></returns>
     public static bool GetValue<T>(string key, Action<T> callback = null) {
         if (!Available(key)) return false;
         MethodInfo methodInfo = GetStaticPublicMethodInfo(GET_VALUE);
@@ -174,6 +269,14 @@ public static class ModSettingAPI {
         return true;
     }
 
+    /// <summary>
+    /// 设置指定key的配置值，单方面通知控件更新
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="value">设置值</param>
+    /// <param name="callback">回调函数返回是否成功</param>
+    /// <typeparam name="T">值类型</typeparam>
+    /// <returns></returns>
     public static bool SetValue<T>(string key, T value, Action<bool> callback = null) {
         if (!Available(key)) return false;
         MethodInfo methodInfo = GetStaticPublicMethodInfo(SET_VALUE);
@@ -183,6 +286,10 @@ public static class ModSettingAPI {
         return true;
     }
 
+    /// <summary>
+    /// 检查是否存在此mod的配置文件
+    /// </summary>
+    /// <returns></returns>
     public static bool HasConfig() {
         if (!Available()) return false;
         MethodInfo methodInfo = GetStaticPublicMethodInfo(HAS_CONFIG);
@@ -190,6 +297,13 @@ public static class ModSettingAPI {
         return (bool)methodInfo.Invoke(null, new object[] { modInfo });
     }
 
+    /// <summary>
+    /// 获取已保存的配置值
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="value">保存的值</param>
+    /// <typeparam name="T">值类型</typeparam>
+    /// <returns></returns>
     public static bool GetSavedValue<T>(string key, out T value) {
         value = default;
         if (!Available(key)) return false;
@@ -204,6 +318,12 @@ public static class ModSettingAPI {
         return result;
     }
 
+    /// <summary>
+    /// 移除指定键对应的UI控件
+    /// </summary>
+    /// <param name="key">控件key</param>
+    /// <param name="callback">回调函数返回操作结果</param>
+    /// <returns></returns>
     public static bool RemoveUI(string key, Action<bool> callback = null) {
         if (!Available(key)) return false;
         return InvokeMethod(REMOVE_UI,
@@ -212,6 +332,11 @@ public static class ModSettingAPI {
             typeof(Action<ModInfo, string, Action<bool>>));
     }
 
+    /// <summary>
+    /// 移除整个模组的UI配置,当禁用此mod时，ModSetting会自动移除相对应的UI，一般来说不需要调用，除非想要主动移除mod所有UI
+    /// </summary>
+    /// <param name="callback">回调函数返回操作结果</param>
+    /// <returns></returns>
     public static bool RemoveMod(Action<bool> callback = null) {
         if (!Available()) return false;
         Type delegateType = typeof(Action<ModInfo, Action<bool>>);
